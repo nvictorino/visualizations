f21023f99be59c563b79b7466b80b2ac52349c36
nvictorino <nvictorino@gmail.com>
2017-08-29 16:37:04 -0400

    organized folders - added HPC map

---
+++ hpc-map/countries.js
@@ -0,0 +1,425 @@
+var countryData = [{
+	"location":"Venezuela", 
+	"latitude": 8.375, 
+	"longitude":-66.445, 
+	"size":10, 
+	"figures": 
+	{
+		"2017": 
+		{
+			"pin":50, 
+			"targeted":7,
+			"reached":3,
+			"funded":2,
+			"required": 5
+		},
+		"2016": 
+		{
+			"pin":8, 
+			"targeted":6,
+			"reached":2,
+			"funded":1,
+			"required": 3
+		}
+	} 
+},
+{
+	"location":"Ecuador", 
+	"latitude": -1.549, 
+	"longitude":-83.694, 
+	"size":10, 
+	"figures": 
+	{
+		"2017": 
+		{
+			"pin":10, 
+			"targeted":7,
+			"reached":3,
+			"funded":2,
+			"required": 5
+		},
+		"2016": 
+		{
+			"pin":8, 
+			"targeted":6,
+			"reached":2,
+			"funded":1,
+			"required": 3
+		}
+	} 
+},
+{
+	"location":"Sierra Leone", 
+	"latitude": 8.375, 
+	"longitude":-66.445, 
+	"size":10, 
+	"figures": 
+	{
+		"2017": 
+		{
+			"pin":10, 
+			"targeted":7,
+			"reached":3,
+			"funded":2,
+			"required": 5
+		},
+		"2016": 
+		{
+			"pin":8, 
+			"targeted":6,
+			"reached":2,
+			"funded":1,
+			"required": 3
+		}
+	} 
+},
+{
+	"location":"Mali", 
+	"latitude": 17.728, 
+	"longitude":-3.988, 
+	"size":10, 
+	"figures": 
+	{
+		"2017": 
+		{
+			"pin":10, 
+			"targeted":7,
+			"reached":3,
+			"funded":2,
+			"required": 5
+		},
+		"2016": 
+		{
+			"pin":8, 
+			"targeted":6,
+			"reached":2,
+			"funded":1,
+			"required": 3
+		}
+	} 
+},
+{
+	"location":"Nigeria", 
+	"latitude": 9.015, 
+	"longitude":8.668, 
+	"size":10, 
+	"figures": 
+	{
+		"2017": 
+		{
+			"pin":10, 
+			"targeted":7,
+			"reached":3,
+			"funded":2,
+			"required": 5
+		},
+		"2016": 
+		{
+			"pin":8, 
+			"targeted":6,
+			"reached":2,
+			"funded":1,
+			"required": 3
+		}
+	} 
+},
+{
+	"location":"Cameroon", 
+	"latitude": 7.406, 
+	"longitude":1272, 
+	"size":30, 
+	"figures": 
+	{
+		"2017": 
+		{
+			"pin":10, 
+			"targeted":7,
+			"reached":3,
+			"funded":2,
+			"required": 5
+		},
+		"2016": 
+		{
+			"pin":8, 
+			"targeted":6,
+			"reached":2,
+			"funded":1,
+			"required": 3
+		}
+	} 
+},
+{
+	"location":"Central African Republic", 
+	"latitude": 6.621, 
+	"longitude":20.929, 
+	"size":20, 
+	"figures": 
+	{
+		"2017": 
+		{
+			"pin":10, 
+			"targeted":7,
+			"reached":3,
+			"funded":2,
+			"required": 5
+		},
+		"2016": 
+		{
+			"pin":8, 
+			"targeted":6,
+			"reached":2,
+			"funded":1,
+			"required": 3
+		}
+	} 
+},
+{
+	"location":"DR Congo", 
+	"latitude": -4.083, 
+	"longitude":21.654, 
+	"size":15, 
+	"figures": 
+	{
+		"2017": 
+		{
+			"pin":10, 
+			"targeted":7,
+			"reached":3,
+			"funded":2,
+			"required": 5
+		},
+		"2016": 
+		{
+			"pin":8, 
+			"targeted":6,
+			"reached":2,
+			"funded":1,
+			"required": 3
+		}
+	} 
+},
+{
+	"location":"South Sudan", 
+	"latitude": 7.886, 
+	"longitude":29.698, 
+	"size":15, 
+	"figures": 
+	{
+		"2017": 
+		{
+			"pin":10, 
+			"targeted":7,
+			"reached":3,
+			"funded":2,
+			"required": 5
+		},
+		"2016": 
+		{
+			"pin":8, 
+			"targeted":6,
+			"reached":2,
+			"funded":1,
+			"required": 3
+		}
+	} 
+},
+{
+	"location":"Somalia", 
+	"latitude": 5.222, 
+	"longitude":46.285, 
+	"size":15, 
+	"figures": 
+	{
+		"2017": 
+		{
+			"pin":10, 
+			"targeted":7,
+			"reached":3,
+			"funded":2,
+			"required": 5
+		},
+		"2016": 
+		{
+			"pin":8, 
+			"targeted":6,
+			"reached":2,
+			"funded":1,
+			"required": 3
+		}
+	} 
+},
+{
+	"location":"Yemen", 
+	"latitude": 15.475, 
+	"longitude":48.153, 
+	"size":15, 
+	"figures": 
+	{
+		"2017": 
+		{
+			"pin":10, 
+			"targeted":7,
+			"reached":3,
+			"funded":2,
+			"required": 5
+		},
+		"2016": 
+		{
+			"pin":8, 
+			"targeted":6,
+			"reached":2,
+			"funded":1,
+			"required": 3
+		}
+	} 
+},
+{
+	"location":"Occupied Palestinian Territory", 
+	"latitude":31.9216, 
+	"longitude":35.2033, 
+	"size":10, 
+	"figures": 
+	{
+		"2017": 
+		{
+			"pin":10, 
+			"targeted":7,
+			"reached":3,
+			"funded":2,
+			"required": 5
+		},
+		"2016": 
+		{
+			"pin":8, 
+			"targeted":6,
+			"reached":2,
+			"funded":1,
+			"required": 3
+		}
+	} 
+},
+{
+	"location":"Iraq", 
+	"latitude":33.320, 
+	"longitude":43.817, 
+	"size":40, 
+	"figures": 
+	{
+		"2017": 
+		{
+			"pin":10, 
+			"targeted":7,
+			"reached":3,
+			"funded":2,
+			"required": 5
+		},
+		"2016": 
+		{
+			"pin":8, 
+			"targeted":6,
+			"reached":2,
+			"funded":1,
+			"required": 3
+		}
+	} 
+},
+{
+	"location":"Syria", 
+	"latitude":34.976, 
+	"longitude":39.056, 
+	"size":30, 
+	"figures": 
+	{
+		"2017": 
+		{
+			"pin":10, 
+			"targeted":7,
+			"reached":3,
+			"funded":2,
+			"required": 5
+		},
+		"2016": 
+		{
+			"pin":8, 
+			"targeted":6,
+			"reached":2,
+			"funded":1,
+			"required": 3
+		}
+	} 
+},
+{
+	"location":"Afghanistan", 
+	"latitude":34.056, 
+	"longitude":67.704, 
+	"size":10, 
+	"figures": 
+	{
+		"2017": 
+		{
+			"pin":10, 
+			"targeted":7,
+			"reached":3,
+			"funded":2,
+			"required": 5
+		},
+		"2016": 
+		{
+			"pin":8, 
+			"targeted":6,
+			"reached":2,
+			"funded":1,
+			"required": 3
+		}
+	} 
+},
+{
+	"location":"Myanmar", 
+	"latitude":19.318, 
+	"longitude":96.671, 
+	"size":30, 
+	"figures": 
+	{
+		"2017": 
+		{
+			"pin":10, 
+			"targeted":7,
+			"reached":3,
+			"funded":2,
+			"required": 5
+		},
+		"2016": 
+		{
+			"pin":8, 
+			"targeted":6,
+			"reached":2,
+			"funded":1,
+			"required": 3
+		}
+	} 
+},
+{
+	"location":"Philippines", 
+	"latitude":12.915, 
+	"longitude":121.748, 
+	"size":30, 
+	"figures": 
+	{
+		"2017": 
+		{
+			"pin":10, 
+			"targeted":7,
+			"reached":3,
+			"funded":2,
+			"required": 5
+		},
+		"2016": 
+		{
+			"pin":8, 
+			"targeted":6,
+			"reached":2,
+			"funded":1,
+			"required": 3
+		}
+	} 
+}]
