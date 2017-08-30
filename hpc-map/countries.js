8317f0e646466d370185700179e6ee986afe65d4
nvictorino <nvictorino@gmail.com>
2017-08-30 16:32:08 -0400

    fix dummy data

--- hpc-map/countries.js
+++ hpc-map/countries.js
@@ -1,183 +1,175 @@
 var countryData = [{
 	"location":"Venezuela", 
 	"latitude": 8.375, 
-	"longitude":-66.445, 
-	"size":10, 
+	"longitude":-66.445,
 	"figures": 
 	{
 		"2017": 
 		{
 			"pin":50, 
-			"targeted":7,
-			"reached":3,
-			"funded":2,
-			"required": 5
+			"targeted":30,
+			"reached":20,
+			"funded":20,
+			"required": 50
 		},
 		"2016": 
 		{
 			"pin":20, 
-			"targeted":6,
-			"reached":2,
-			"funded":1,
-			"required": 3
+			"targeted":15,
+			"reached":10,
+			"funded":10,
+			"required": 30
 		}
 	} 
 },
 {
 	"location":"Ecuador", 
 	"latitude": -1.549, 
-	"longitude":-83.694, 
-	"size":10, 
+	"longitude":-83.694,
 	"figures": 
 	{
 		"2017": 
 		{
 			"pin":20, 
-			"targeted":7,
-			"reached":3,
-			"funded":2,
-			"required": 5
+			"targeted":15,
+			"reached":8,
+			"funded":10,
+			"required": 20
 		},
 		"2016": 
 		{
 			"pin":17, 
-			"targeted":6,
-			"reached":2,
-			"funded":1,
-			"required": 3
+			"targeted":12,
+			"reached":8,
+			"funded":15,
+			"required": 35
 		}
 	} 
 },
 {
 	"location":"Sierra Leone", 
 	"latitude": 8.381, 
-	"longitude":-11.886, 
-	"size":10, 
+	"longitude":-11.886,
 	"figures": 
 	{
 		"2017": 
 		{
 			"pin":5, 
-			"targeted":7,
+			"targeted":4,
 			"reached":3,
-			"funded":2,
-			"required": 5
+			"funded":10,
+			"required": 15
 		},
 		"2016": 
 		{
 			"pin":35, 
-			"targeted":6,
-			"reached":2,
-			"funded":1,
-			"required": 3
+			"targeted":25,
+			"reached":12,
+			"funded":25,
+			"required": 30
 		}
 	} 
 },
 {
 	"location":"Mali", 
 	"latitude": 17.728, 
-	"longitude":-3.988, 
-	"size":10, 
+	"longitude":-3.988,
 	"figures": 
 	{
 		"2017": 
 		{
 			"pin":20, 
-			"targeted":7,
-			"reached":3,
-			"funded":2,
-			"required": 5
+			"targeted":15,
+			"reached":8,
+			"funded":5,
+			"required": 10
 		},
 		"2016": 
 		{
 			"pin":6, 
-			"targeted":6,
-			"reached":2,
-			"funded":1,
-			"required": 3
+			"targeted":4,
+			"reached":3,
+			"funded":10,
+			"required": 15
 		}
 	} 
 },
 {
 	"location":"Nigeria", 
 	"latitude": 9.015, 
-	"longitude":8.668, 
-	"size":10, 
+	"longitude":8.668,
 	"figures": 
 	{
 		"2017": 
 		{
 			"pin":33, 
-			"targeted":7,
-			"reached":3,
-			"funded":2,
-			"required": 5
+			"targeted":20,
+			"reached":15,
+			"funded":8,
+			"required": 10
 		},
 		"2016": 
 		{
 			"pin":47, 
-			"targeted":6,
-			"reached":2,
-			"funded":1,
-			"required": 3
+			"targeted":35,
+			"reached":20,
+			"funded":17,
+			"required": 20
 		}
 	} 
 },
 {
 	"location":"Cameroon", 
 	"latitude": 7.406, 
-	"longitude":1272, 
-	"size":30, 
+	"longitude":1272,
 	"figures": 
 	{
 		"2017": 
 		{
 			"pin":18, 
-			"targeted":7,
-			"reached":3,
-			"funded":2,
-			"required": 5
+			"targeted":12,
+			"reached":10,
+			"funded":10,
+			"required": 15
 		},
 		"2016": 
 		{
 			"pin":14, 
-			"targeted":6,
-			"reached":2,
-			"funded":1,
-			"required": 3
+			"targeted":10,
+			"reached":7,
+			"funded":15,
+			"required": 20
 		}
 	} 
 },
 {
 	"location":"Central African Republic", 
 	"latitude": 6.621, 
-	"longitude":20.929, 
-	"size":20, 
+	"longitude":20.929,
 	"figures": 
 	{
 		"2017": 
 		{
 			"pin":60, 
-			"targeted":7,
-			"reached":3,
-			"funded":2,
-			"required": 5
+			"targeted":40,
+			"reached":30,
+			"funded":5,
+			"required": 10
 		},
 		"2016": 
 		{
 			"pin":40, 
-			"targeted":6,
-			"reached":2,
-			"funded":1,
-			"required": 3
+			"targeted":25,
+			"reached":15,
+			"funded":7,
+			"required": 14
 		}
 	} 
 },
 {
 	"location":"DR Congo", 
 	"latitude": -4.083, 
-	"longitude":21.654, 
-	"size":15, 
+	"longitude":21.654,
 	"figures": 
 	{
 		"2017": 
@@ -185,99 +177,95 @@ var countryData = [{
 			"pin":10, 
 			"targeted":7,
 			"reached":3,
-			"funded":2,
-			"required": 5
+			"funded":28,
+			"required": 30
 		},
 		"2016": 
 		{
 			"pin":8, 
 			"targeted":6,
 			"reached":2,
-			"funded":1,
-			"required": 3
+			"funded":10,
+			"required": 13
 		}
 	} 
 },
 {
 	"location":"South Sudan", 
 	"latitude": 7.886, 
-	"longitude":29.698, 
-	"size":15, 
+	"longitude":29.698,
 	"figures": 
 	{
 		"2017": 
 		{
-			"pin":10, 
-			"targeted":7,
-			"reached":3,
-			"funded":2,
-			"required": 5
+			"pin":20, 
+			"targeted":17,
+			"reached":13,
+			"funded":21,
+			"required": 25
 		},
 		"2016": 
 		{
-			"pin":8, 
-			"targeted":6,
-			"reached":2,
-			"funded":1,
-			"required": 3
+			"pin":18, 
+			"targeted":16,
+			"reached":12,
+			"funded":10,
+			"required": 14
 		}
 	} 
 },
 {
 	"location":"Somalia", 
 	"latitude": 5.222, 
-	"longitude":46.285, 
-	"size":15, 
+	"longitude":46.285,
 	"figures": 
 	{
 		"2017": 
 		{
 			"pin":20, 
-			"targeted":7,
-			"reached":3,
-			"funded":2,
-			"required": 5
+			"targeted":17,
+			"reached":13,
+			"funded":15,
+			"required": 18
 		},
 		"2016": 
 		{
-			"pin":8, 
-			"targeted":6,
-			"reached":2,
-			"funded":1,
-			"required": 3
+			"pin":18, 
+			"targeted":16,
+			"reached":12,
+			"funded":12,
+			"required": 16
 		}
 	} 
 },
 {
 	"location":"Yemen", 
 	"latitude": 15.475, 
-	"longitude":48.153, 
-	"size":15, 
+	"longitude":48.153,
 	"figures": 
 	{
 		"2017": 
 		{
-			"pin":35, 
-			"targeted":7,
-			"reached":3,
-			"funded":2,
-			"required": 5
+			"pin":20, 
+			"targeted":17,
+			"reached":13,
+			"funded":40,
+			"required": 43
 		},
 		"2016": 
 		{
-			"pin":8, 
-			"targeted":6,
-			"reached":2,
-			"funded":1,
-			"required": 3
+			"pin":18, 
+			"targeted":16,
+			"reached":10,
+			"funded":33,
+			"required": 33
 		}
 	} 
 },
 {
 	"location":"Occupied Palestinian Territory", 
 	"latitude":31.9216, 
-	"longitude":35.2033, 
-	"size":10, 
+	"longitude":35.2033,
 	"figures": 
 	{
 		"2017": 
@@ -285,16 +273,16 @@ var countryData = [{
 			"pin":5, 
 			"targeted":7,
 			"reached":3,
-			"funded":2,
-			"required": 5
+			"funded":12,
+			"required": 15
 		},
 		"2016": 
 		{
 			"pin":8, 
 			"targeted":6,
 			"reached":2,
-			"funded":1,
-			"required": 3
+			"funded":12,
+			"required": 15
 		}
 	} 
 },
@@ -302,57 +290,54 @@ var countryData = [{
 	"location":"Iraq", 
 	"latitude":33.320, 
 	"longitude":43.817, 
-	"size":40, 
 	"figures": 
 	{
 		"2017": 
 		{
-			"pin":15, 
-			"targeted":7,
-			"reached":3,
-			"funded":2,
-			"required": 5
+			"pin":30, 
+			"targeted":25,
+			"reached":20,
+			"funded":7,
+			"required":10
 		},
 		"2016": 
 		{
-			"pin":8, 
-			"targeted":6,
-			"reached":2,
-			"funded":1,
-			"required": 3
+			"pin":40, 
+			"targeted":35,
+			"reached":20,
+			"funded":13,
+			"required": 17
 		}
 	} 
 },
 {
 	"location":"Syria", 
 	"latitude":34.976, 
-	"longitude":39.056, 
-	"size":30, 
+	"longitude":39.056,
 	"figures": 
 	{
 		"2017": 
 		{
-			"pin":28, 
-			"targeted":7,
-			"reached":3,
-			"funded":2,
-			"required": 5
+			"pin":30, 
+			"targeted":28,
+			"reached":25,
+			"funded":20,
+			"required": 25
 		},
 		"2016": 
 		{
 			"pin":18, 
 			"targeted":6,
 			"reached":2,
-			"funded":1,
-			"required": 3
+			"funded":10,
+			"required": 30
 		}
 	} 
 },
 {
 	"location":"Afghanistan", 
 	"latitude":34.056, 
-	"longitude":67.704, 
-	"size":10, 
+	"longitude":67.704,
 	"figures": 
 	{
 		"2017": 
@@ -360,49 +345,47 @@ var countryData = [{
 			"pin":10, 
 			"targeted":7,
 			"reached":3,
-			"funded":2,
-			"required": 5
+			"funded":20,
+			"required": 40
 		},
 		"2016": 
 		{
 			"pin":8, 
 			"targeted":6,
 			"reached":2,
-			"funded":1,
-			"required": 3
+			"funded":20,
+			"required": 30
 		}
 	} 
 },
 {
 	"location":"Myanmar", 
 	"latitude":19.318, 
-	"longitude":96.671, 
-	"size":30, 
+	"longitude":96.671,
 	"figures": 
 	{
 		"2017": 
 		{
 			"pin":15, 
-			"targeted":7,
-			"reached":3,
-			"funded":2,
-			"required": 5
+			"targeted":13,
+			"reached":7,
+			"funded":7,
+			"required": 18
 		},
 		"2016": 
 		{
 			"pin":19, 
 			"targeted":6,
-			"reached":2,
-			"funded":1,
-			"required": 3
+			"reached":4,
+			"funded":8,
+			"required": 16
 		}
 	} 
 },
 {
 	"location":"Philippines", 
 	"latitude":12.915, 
-	"longitude":121.748, 
-	"size":30, 
+	"longitude":121.748,
 	"figures": 
 	{
 		"2017": 
@@ -410,16 +393,16 @@ var countryData = [{
 			"pin":20, 
 			"targeted":7,
 			"reached":3,
-			"funded":2,
-			"required": 5
+			"funded":20,
+			"required": 30
 		},
 		"2016": 
 		{
 			"pin":15, 
 			"targeted":6,
 			"reached":2,
-			"funded":1,
-			"required": 3
+			"funded":10,
+			"required": 30
 		}
 	} 
 }]
